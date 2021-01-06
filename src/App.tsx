19441e109e3ef0c8e0350279a8c75227d9c5a806
Marcio <marcio@zimbres.com>
2021-01-06 15:52:35 -0300

    Add Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
 
       <Navbar />
-
+      <Home />
     </div>
   );
 }
