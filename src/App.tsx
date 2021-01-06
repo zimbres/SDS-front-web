e3d145f3a506412aca2e75aa9307d6f2cba2afe5
Marcio <marcio@zimbres.com>
2021-01-06 16:22:35 -0300

    Add Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css';
-import Home from './Home';
 import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div className="App">
 
-      <Navbar />
-      <Home />
-    </div>
+      <Routes />
   );
 }
 
